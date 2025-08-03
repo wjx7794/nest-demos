4171420c19b823fb224c46cc50965a48930d9932
wjx7794 <wjx7794@163.com>
2025-08-03 11:35:55 +0800

    feat: tz

--- demo8/src/main.ts
+++ demo8/src/main.ts
@@ -6,7 +6,7 @@ import { grpcClientOptions } from './grpc-client.options';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
-
+  console.log('start');
   await app.startAllMicroservices();
   await app.listen(3001);
 }
